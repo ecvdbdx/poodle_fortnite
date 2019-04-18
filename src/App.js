9f317e7aa2d38472b841d301a2f2fdd6f2a4e2b8
Gaétan Nicolas <gnicolas@appstud.com>
2019-04-18 17:09:37 +0200

    Add folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
-import Player from './Player'
-import News from './News'
+import Player from './Player/Player'
+import News from './News/News'
 
 class App extends Component {
   render() {
@@ -11,9 +11,10 @@ class App extends Component {
         <div className="App">
           <h1>FORTNITE</h1>
           <Link to="/player/">Player</Link>
+          <Link to="/news/">News</Link>
         </div>
 
-        <Route path="/" component={News} />
+        <Route path="/news" component={News} />
         <Route path="/player/" component={Player} />
       </Router>
     );
